3d6acff9180b158d516afcdaf79121b0424253b8
shkbhbb <shkbhbb@gmail.com>
2018-02-17 11:50:09 +0330

    init

---
+++ thebestutil/src/main/java/ir/shkbhbb/thebestutil/Util.java
@@ -0,0 +1,15 @@
+package ir.shkbhbb.thebestutil;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by shkbhbb on 1/25/18.
+ */
+
+public class Util {
+
+  public static void showToast(Context context) {
+    Toast.makeText(context, "done!", Toast.LENGTH_SHORT).show();
+  }
+}
